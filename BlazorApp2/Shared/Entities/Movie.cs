a50abb497f8ef70da861b0d2a385a6a7252790f2
redmoongames <redmoon.thebest@gmail.com>
2020-04-19 14:17:05 +0300

    Add project files.

---
+++ BlazorApp2/Shared/Entities/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorApp2.Shared.Entities
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+
+    }
+
+}
